01c20eefab19c8f0c41d82d4f01bc0134ad6f6b3
Seth Kingsbury <swk22@uclive.ac.nz>
2022-07-19 14:18:18 +1200

    introduction to context.

--- src/App.js
+++ src/App.js
@@ -8,47 +8,50 @@ import FeedbackStats from './components/FeedbackStats';
 import FeedbackForm from './components/FeedbackForm';
 import AboutPage from './pages/AboutPage';
 import AboutIconLink from './components/AboutIconLink';
+import { FeedbackProvider } from './context/FeedbackContext';
 
 function App() {
-	const [feedback, setFeedback] = useState(FeedbackData);
+  const [feedback, setFeedback] = useState(FeedbackData);
 
-	const deleteFeedback = (id) => {
-		if (window.confirm('Are you sure you want to delete?')) {
-			setFeedback(feedback.filter((item) => item.id !== id));
-		}
-	};
+  const deleteFeedback = (id) => {
+    if (window.confirm('Are you sure you want to delete?')) {
+      setFeedback(feedback.filter((item) => item.id !== id));
+    }
+  };
 
-	const addFeedback = (newFeedback) => {
-		newFeedback.id = uuidv4();
-		setFeedback([newFeedback, ...feedback]);
-	};
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4();
+    setFeedback([newFeedback, ...feedback]);
+  };
 
-	return (
-		<Router>
-			<Header />
-			<div className='container'>
-				<Routes>
-					<Route
-						exact
-						path='/'
-						element={
-							<>
-								<FeedbackForm handleAdd={addFeedback} />
-								<FeedbackStats feedback={feedback} />
-								<FeedbackList
-									feedback={feedback}
-									deleteFeedback={deleteFeedback}
-								/>
-							</>
-						}
-					/>
+  return (
+    <FeedbackProvider>
+      <Router>
+        <Header />
+        <div className='container'>
+          <Routes>
+            <Route
+              exact
+              path='/'
+              element={
+                <>
+                  <FeedbackForm handleAdd={addFeedback} />
+                  <FeedbackStats feedback={feedback} />
+                  <FeedbackList
+                    feedback={feedback}
+                    deleteFeedback={deleteFeedback}
+                  />
+                </>
+              }
+            />
 
-					<Route path='/about' element={<AboutPage />} />
-				</Routes>
-				<AboutIconLink />
-			</div>
-		</Router>
-	);
+            <Route path='/about' element={<AboutPage />} />
+          </Routes>
+          <AboutIconLink />
+        </div>
+      </Router>
+    </FeedbackProvider>
+  );
 }
 
 export default App;
